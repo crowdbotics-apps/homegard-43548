ca6a30f9cc16866fbee9d9c3aad7ce8fbf4846ef
Crowdbotics <team@crowdbotics.com>
2023-09-24 22:55:24 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "homegard-43548",
+  url: "https://homegard-43548.botics.co",
+  api: "https://homegard-43548.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
